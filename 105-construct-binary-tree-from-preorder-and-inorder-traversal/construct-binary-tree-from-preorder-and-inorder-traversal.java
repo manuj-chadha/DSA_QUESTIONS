49fbb0028f19df768a592c4e1cdfbac010b9448e
manuj-chadha <160269648+manuj-chadha@users.noreply.github.com>
2025-01-16 14:13:50 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (25.59%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -14,21 +14,40 @@
  * }
  */
 class Solution {
+    private int pre=0;
+    private int in=0;
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        if(preorder.length==0){
+        return buildTree(preorder, inorder, Integer.MIN_VALUE);
+    }
+    private TreeNode buildTree(int[] preorder, int[] inorder, int stop){
+        if(pre>=preorder.length){
             return null;
         }
-        int root=preorder[0];
-        int index=0;
-        TreeNode node=new TreeNode(root);
-        for(int i=0;i<inorder.length;i++){
-            if(inorder[i]==root){
-                index=i;
-            }
+        if(inorder[in]==stop){
+            in++;
+            return null;
         }
-        node.left=buildTree(Arrays.copyOfRange(preorder, 1, index+1), Arrays.copyOfRange(inorder, 0, index+1));
-        node.right=buildTree(Arrays.copyOfRange(preorder, index+1, preorder.length), Arrays.copyOfRange(inorder, index+1, inorder.length));
-
+        TreeNode node=new TreeNode(preorder[pre++]);
+        node.left=buildTree(preorder, inorder, node.val);
+        node.right=buildTree(preorder, inorder, stop);
         return node;
     }
+
+    // public TreeNode buildTree(int[] preorder, int[] inorder) {
+    //     if(preorder.length==0){
+    //         return null;
+    //     }
+    //     int root=preorder[0];
+    //     int index=0;
+    //     TreeNode node=new TreeNode(root);
+    //     for(int i=0;i<inorder.length;i++){
+    //         if(inorder[i]==root){
+    //             index=i;
+    //         }
+    //     }
+    //     node.left=buildTree(Arrays.copyOfRange(preorder, 1, index+1), Arrays.copyOfRange(inorder, 0, index+1));
+    //     node.right=buildTree(Arrays.copyOfRange(preorder, index+1, preorder.length), Arrays.copyOfRange(inorder, index+1, inorder.length));
+
+    //     return node;
+    // }
 }
