0711d3a9c96d5aafe1a4b7fb16b563091fffc74d
manuj-chadha <160269648+manuj-chadha@users.noreply.github.com>
2025-01-16 19:14:43 +0530

    Time: 8 ms (6.14%) | Memory: 84 MB (5.41%) - LeetSync

---
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        if(postorder.length<=0){
+            return null;
+        }
+        int val=postorder[postorder.length-1];
+        int index=0;
+        for(int i=0;i<inorder.length;i++){
+            if(inorder[i]==val){
+                index=i;
+            }
+        }
+        TreeNode root=new TreeNode(val);
+        root.left=buildTree(Arrays.copyOfRange(inorder, 0, index), Arrays.copyOfRange(postorder, 0, index));
+        root.right=buildTree(Arrays.copyOfRange(inorder, index+1, inorder.length), Arrays.copyOfRange(postorder, index, postorder.length-1));
+        return root;
+    }
+}
