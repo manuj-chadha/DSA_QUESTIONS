bab832d657be7ef815708b332302200a50fb4032
manuj-chadha <160269648+manuj-chadha@users.noreply.github.com>
2025-02-01 09:28:01 +0530

    Time: 2 ms (64.83%) | Memory: 41.6 MB (95.09%) - LeetSync

--- 389-find-the-difference/find-the-difference.java
+++ 389-find-the-difference/find-the-difference.java
@@ -8,29 +8,4 @@ class Solution {
         }
         return ch;
     }
-    // public char findTheDifference(String s, String t) {
-    //     Hashtable<Character, Integer> hash=new Hashtable<>();
-    //     for(int i=0;i<t.length();i++){
-    //         char ch=t.charAt(i);
-    //         if(hash.containsKey(ch)){
-    //             hash.put(ch, hash.get(ch)+1);
-    //         }
-    //         else{
-    //             hash.put(ch, 1);
-    //         }
-    //     }
-    //     for(int i=0;i<s.length();i++){
-    //         char ch=s.charAt(i);
-    //         int val=hash.get(ch);
-    //         if(val==1){
-    //             hash.remove(ch);
-    //         }
-    //         else{
-    //             hash.put(ch, val-1);
-    //         }
-    //     }
-    //     Character singleValue = hash.keySet().iterator().next();
-    //     return singleValue;
-    // }
-
 }
