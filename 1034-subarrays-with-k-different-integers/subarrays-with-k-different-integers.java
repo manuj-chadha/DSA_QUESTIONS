332e106314ee6d674594b70cf3c36c77dffbde26
manuj-chadha <160269648+manuj-chadha@users.noreply.github.com>
2025-02-11 16:42:31 +0530

    Time: 57 ms (6.47%) | Memory: 45.3 MB (99.26%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
@@ -1,25 +1,53 @@
 class Solution {
     public int subarraysWithKDistinct(int[] nums, int k) {
-        return atleast(nums, k)-atleast(nums, k+1);
+        return atmost(nums, k)-atmost(nums, k-1);
     }
-    private int atleast(int[] nums, int k){
+    public int atmost(int[] nums, int k){
         int start=0;
         int end=0;
-        int total=0;
+        int size=nums.length;
         int unique=0;
-        int length=nums.length;
+        int count=0;
         HashMap<Integer, Integer> map=new HashMap<>();
-        while(end<length){
+        while(end<size){
             map.put(nums[end], map.getOrDefault(nums[end], 0)+1);
             if(map.get(nums[end])==1) unique++;
-            while(unique>=k){
-                total+=length-end;
+            while(unique>k){
                 map.put(nums[start], map.get(nums[start])-1);
                 if(map.get(nums[start])==0) unique--;
+                
                 start++;
             }
+            count+=end-start+1;
             end++;
         }
-        return total;
+        return count;
     }
+
+
+
+
+    // public int subarraysWithKDistinct(int[] nums, int k) {
+    //     return atleast(nums, k)-atleast(nums, k+1);
+    // }
+    // private int atleast(int[] nums, int k){
+    //     int start=0;
+    //     int end=0;
+    //     int total=0;
+    //     int unique=0;
+    //     int length=nums.length;
+    //     HashMap<Integer, Integer> map=new HashMap<>();
+    //     while(end<length){
+    //         map.put(nums[end], map.getOrDefault(nums[end], 0)+1);
+    //         if(map.get(nums[end])==1) unique++;
+    //         while(unique>=k){
+    //             total+=length-end;
+    //             map.put(nums[start], map.get(nums[start])-1);
+    //             if(map.get(nums[start])==0) unique--;
+    //             start++;
+    //         }
+    //         end++;
+    //     }
+    //     return total;
+    // }
 }
