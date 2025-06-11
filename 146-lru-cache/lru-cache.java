cdbfb686ee4d57f96cc64d6184425372faaa57ba
Manuj Chadha <manujchadha7777@gmail.com>
2025-06-11 17:53:42 +0530

    Time: 49 ms (40.92%) | Memory: 113.7 MB (60.98%) - LeetSync

--- 146-lru-cache/lru-cache.java
+++ 146-lru-cache/lru-cache.java
@@ -1,25 +1,25 @@
 class LRUCache {
     int capacity=0;
-    LinkedHashMap<Integer, Integer> hashMap;
+    LinkedHashMap<Integer, Integer> map;
+
+
     public LRUCache(int capacity) {
         this.capacity=capacity;
-        hashMap=new LinkedHashMap<>(capacity, 0.75f , true);
+        map=new LinkedHashMap<>(capacity, 0.75f , true);
     }
-
+    
     public int get(int key) {
-        return hashMap.getOrDefault(key, -1);
+        return map.getOrDefault(key, -1);
     }
-
+    
     public void put(int key, int value) {
-        if(hashMap.containsKey(key)){
-            hashMap.put(key, value);
-        }
-        else{
-            if (hashMap.size() == capacity) {
-                Integer firstKey = hashMap.keySet().iterator().next();
-                hashMap.remove(firstKey);
+        if(map.containsKey(key)) map.put(key, value);
+        else {
+            if(map.size()==capacity){
+                Integer firstKey=map.keySet().iterator().next();
+                map.remove(firstKey);
             }
-            hashMap.put(key, value);
+            map.put(key, value);
         }
     }
 }
