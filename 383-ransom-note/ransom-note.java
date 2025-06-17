f357475a09c0f39d2b275e07dfe265d55bb41935
Manuj Chadha <manujchadha7777@gmail.com>
2025-06-17 14:57:40 +0530

    Time: 1 ms (99.32%) | Memory: 44.9 MB (67.20%) - LeetSync

--- 383-ransom-note/ransom-note.java
+++ 383-ransom-note/ransom-note.java
@@ -1,20 +1,15 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        int[] letterCount = new int[26];
+        int[] letterCount=new int[26];
 
-        // Count occurrences of each letter in magazine
-        for (char ch : magazine.toCharArray()) {
-            letterCount[ch - 'a']++;
+        for(char ch: magazine.toCharArray()){
+            letterCount[ch-'a']++;
         }
-
-        // Check if we can construct ransomNote from magazine
-        for (char ch : ransomNote.toCharArray()) {
-            if (letterCount[ch - 'a'] == 0) {
-                return false;  // Letter not available
-            }
-            letterCount[ch - 'a']--;  // Use the letter
+        for(char ch: ransomNote.toCharArray()){
+            if(letterCount[ch-'a']==0) return false;
+            letterCount[ch-'a']--;
         }
-
         return true;
+
     }
 }
