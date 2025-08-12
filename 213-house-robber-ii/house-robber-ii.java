1e2aafbd92a7ecfd9a3344d35fdd85b0eb0f1c91
Manuj Chadha <manujchadha7777@gmail.com>
2025-08-12 23:35:56 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (18.06%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -2,21 +2,18 @@ class Solution {
     public int rob(int[] nums) {
         int n=nums.length;
         if(n==1) return nums[0];
-        if(n==2) return Math.max(nums[0], nums[1]);
-        return Math.max(robber(nums, 0, n-2), robber(nums, 1, n-1));
+        return Math.max(rob(nums, 0, n-2), rob(nums, 1, n-1));
     }
-    int robber(int[] nums, int start, int end){
-        int len=end-start+1;
-        int[] dp=new int[len];
-        dp[len-1]=nums[end];
-        if(len>1){
-            dp[len-2]=Math.max(nums[end], nums[end-1]);
+    private int rob(int[] nums, int start, int end){
+        if (start == end) return nums[start];
+        int iminus2=nums[start];
+        int iminus1=Math.max(nums[start], nums[start+1]);
+        for(int i=start+2;i<=end;i++){
+            int left=iminus1;
+            int right=nums[i]+iminus2;
+            iminus2=iminus1;
+            iminus1=Math.max(left, right);
         }
-        for(int i=len-3;i>=0;i--){
-            dp[i]=Math.max(dp[i+1], dp[i+2]+nums[start+i]);
-        }
-        return dp[0];
-
+        return iminus1;
     }
-
 }
