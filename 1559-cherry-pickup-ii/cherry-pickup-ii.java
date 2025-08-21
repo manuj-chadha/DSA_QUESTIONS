719d8fa7d69c8a6d5a8a247187eac2f23d9af833
Manuj Chadha <manujchadha7777@gmail.com>
2025-08-21 12:39:01 +0530

    Time: 21 ms (94.20%) | Memory: 44.7 MB (97.25%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.java
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.java
@@ -1,32 +1,35 @@
 class Solution {
-    int[][][] dp;
     public int cherryPickup(int[][] grid) {
         int rows=grid.length;
         int cols=grid[0].length;
-        dp=new int[rows][cols][cols];
+        int[][] dp=new int[cols][cols];
         for(int i=0;i<cols;i++){
             for(int j=0;j<cols;j++){
-                if(i==j) dp[rows-1][i][j]=grid[rows-1][i];
-                else dp[rows-1][i][j]=grid[rows-1][i]+grid[rows-1][j];
+                if(i==j) dp[i][j]=grid[rows-1][i];
+                else dp[i][j]=grid[rows-1][i]+grid[rows-1][j];
             }
         }
         
         for(int row=rows-2;row>=0;row--){
+            int[][] curr=new int[cols][cols];
             for(int i=0;i<cols;i++){
                 for(int j=0;j<cols;j++){
                     int result=Integer.MIN_VALUE;
                     for(int r=-1;r<2;r++){
                         for(int c=-1;c<2;c++){
                             if((i+r)>=0 && (i+r)<cols && (j+c)>=0 && (j+c)<cols) {
-                                result=Math.max(result, dp[row+1][i+r][j+c]);
+                                result=Math.max(result, dp[i+r][j+c]);
                             }
                         }
                     }
-                    if(i==j) dp[row][i][j]=grid[row][i]+result;
-                    else dp[row][i][j]=grid[row][i]+grid[row][j]+result;
+                    if(i==j) curr[i][j]=grid[row][i]+result;
+                    else curr[i][j]=grid[row][i]+grid[row][j]+result;
                 }
             }
+            int[][] temp=curr;
+            curr=dp;
+            dp=temp;
         }
-        return dp[0][0][cols-1];
+        return dp[0][cols-1];
     }
 }
