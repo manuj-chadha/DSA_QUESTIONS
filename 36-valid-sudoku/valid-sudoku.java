89efc97e2dfa8b0500d4f8ef05073ff7a11d7cda
Manuj Chadha <manujchadha7777@gmail.com>
2025-09-25 09:57:44 +0530

    Time: 1 ms (99.62%) | Memory: 44.8 MB (27.34%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -2,26 +2,33 @@ class Solution {
     public boolean isValidSudoku(char[][] board) {
         for(int i=0;i<9;i++){
             for(int j=0;j<9;j++){
-                char ch=board[i][j];
-                if(ch!='.'){
-                    if(!validate(board, ch, i, j)) return false;
-                } 
+                if(board[i][j]!='.'){
+                    if(!validate(board, board[i][j], i, j)) return false;
+                }
             }
         }
         return true;
     }
-    public boolean validate(char[][] board, char digit, int row, int column){
+    private boolean validate(char[][] board, char val, int row, int col){
         for(int i=0;i<9;i++){
-            if(column!=i && board[row][i]==digit) return false;
+            if(i!=col && board[row][i]==val){
+                return false;
+            }
         }
         for(int i=0;i<9;i++){
-            if(row!=i && board[i][column]==digit) return false;
+            if(i!=row && board[i][col]==val){
+                return false;
+            }
         }
         int r=row-row%3;
-        int c=column-column%3;
-        for(int i=r;i<(r+3);i++){
-            for(int j=c;j<(c+3);j++){
-                if((i!=row || j!=column) && board[i][j]==digit) return false;
+        int c=col-col%3;
+        for(int i=0;i<3;i++){
+            int newR=r+i;
+            for(int j=0;j<3;j++){
+                int newC=c+j;
+                if(newR!=row && newC!=col && board[newR][newC]==val){
+                    return false;
+                }
             }
         }
         return true;
