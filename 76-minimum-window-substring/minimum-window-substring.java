a8e44ebd81cde417c280c9d98f845a5112b65d56
Manuj Chadha <manujchadha7777@gmail.com>
2026-01-29 23:54:48 +0530

    Time: 22 ms (28.44%) | Memory: 48.2 MB (8.38%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -2,37 +2,47 @@ class Solution {
     public String minWindow(String s, String t) {
         if (t.length() > s.length()) return "";
 
-        int[] need = new int[128];
-        for (char c : t.toCharArray()) {
-            need[c]++;
+        int left = 0;
+        int right = 0;
+
+        HashMap<Character, Integer> need = new HashMap<>();
+        for (int i = 0; i < t.length(); i++) {
+            need.put(t.charAt(i), need.getOrDefault(t.charAt(i), 0) + 1);
         }
 
-        int required = t.length();
-        int left = 0, right = 0;
-        int minLen = Integer.MAX_VALUE;
+        HashMap<Character, Integer> curr = new HashMap<>();
+        int required = need.size();
+        int formed = 0;
+
+        int min = Integer.MAX_VALUE;
         int start = 0;
 
         while (right < s.length()) {
-            char c = s.charAt(right);
-            if (need[c] > 0) required--;
-            need[c]--;
-            right++;
+            char ch = s.charAt(right);
+            curr.put(ch, curr.getOrDefault(ch, 0) + 1);
 
-            while (required == 0) {
-                if (right - left < minLen) {
-                    minLen = right - left;
+            if (need.containsKey(ch) && need.get(ch).equals(curr.get(ch))) {
+                formed++;
+            }
+
+            while (formed == required) {
+                if (right - left + 1 < min) {
+                    min = right - left + 1;
                     start = left;
                 }
 
-                char lc = s.charAt(left);
-                need[lc]++;
-                if (need[lc] > 0) required++;
+                char chLeft = s.charAt(left);
+                curr.put(chLeft, curr.get(chLeft) - 1);
+
+                if (need.containsKey(chLeft) && curr.get(chLeft) < need.get(chLeft)) {
+                    formed--;
+                }
+
                 left++;
             }
+            right++;
         }
 
-        return minLen == Integer.MAX_VALUE 
-            ? "" 
-            : s.substring(start, start + minLen);
+        return min == Integer.MAX_VALUE ? "" : s.substring(start, start + min);
     }
 }
