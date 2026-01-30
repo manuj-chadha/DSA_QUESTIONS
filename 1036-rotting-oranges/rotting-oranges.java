28c8c41da9331030036dbc15baeccb74e6c888d8
Manuj Chadha <manujchadha7777@gmail.com>
2026-01-30 21:43:49 +0530

    Time: 2 ms (90.90%) | Memory: 44.6 MB (14.18%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,36 +1,36 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        int time=0;
+        int minutes=0;
         Queue<int[]> queue=new LinkedList<>();
-        for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[0].length;j++){
-                if(grid[i][j]==2) queue.offer(new int[]{i, j});
+        for(int i=0;i<grid.length;i++) {
+            for(int j=0;j<grid[0].length;j++) {
+                if(grid[i][j]==2) queue.add(new int[]{i, j});
             }
         }
+        int[] dx={1, -1, 0, 0};
+        int[] dy={0, 0, 1, -1};
         while(!queue.isEmpty()){
             int size=queue.size();
-            boolean rotted=false;
-            for(int k=0;k<size;k++){
-                int[] arr=queue.poll();
-                int[] dx={-1, 0, 0, 1};
-                int[] dy={0, -1, 1, 0};
-                for(int i=0;i<4;i++){
-                    int nextR=arr[0]+dx[i];
-                    int nextC=arr[1]+dy[i];
-                    if(nextR>=0 && nextR<grid.length && nextC>=0 && nextC<grid[0].length && grid[nextR][nextC]==1){
-                        grid[nextR][nextC]=2;
-                        queue.offer(new int[]{nextR, nextC});
-                        rotted=true;
+            for(int i=0;i<size;i++){
+                int[] orange=queue.poll();
+                for(int a=0;a<4;a++) {
+                    int newX=orange[0]+dx[a];
+                    int newY=orange[1]+dy[a];
+                    if(newX>=0 && newX<grid.length && newY>=0 && newY<grid[0].length){
+                        if(grid[newX][newY]==1){
+                            grid[newX][newY]=2;
+                            queue.add(new int[]{newX, newY});
+                        }
                     }
                 }
             }
-            if(rotted) time++;
+            if(!queue.isEmpty()) minutes++;
         }
-        for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[0].length;j++){
-                if(grid[i][j]==1) return -1;
+        for(int[] arr: grid){
+            for(int val: arr){
+                if(val==1) return -1;
             }
         }
-        return time;
+        return minutes;
     }
 }
