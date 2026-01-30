7cb65d3eb7bb34dfb8d177ffe86ff984daeac81c
Manuj Chadha <manujchadha7777@gmail.com>
2026-01-30 21:11:39 +0530

    Time: 0 ms (100.00%) | Memory: 47.1 MB (8.29%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,24 +1,23 @@
 class Solution {
-    boolean[][] visited;
+    int oldColor;
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        visited=new boolean[image.length][image[0].length];
+        if(color==image[sr][sc]) return image;
+        oldColor=image[sr][sc];
         fillColor(image, sr, sc, color);
         return image;
     }
     private void fillColor(int[][] arr, int sr, int sc, int color) {
-        visited[sr][sc]=true;
-
+        arr[sr][sc]=color;
         int[] dx={0, -1, 0, 1};
         int[] dy={-1, 0, 1, 0};
         for(int a=0;a<4;a++) {
             int newX=sr+dx[a];
             int newY=sc+dy[a];
             if(newX>=0 && newX<arr.length && newY>=0 && newY<arr[0].length){
-                if(!visited[newX][newY] && arr[newX][newY]==arr[sr][sc]){
+                if(arr[newX][newY]!=color && arr[newX][newY]==oldColor){
                     fillColor(arr, newX, newY, color);
                 }
             }
         }
-        arr[sr][sc]=color;
     }
 }
