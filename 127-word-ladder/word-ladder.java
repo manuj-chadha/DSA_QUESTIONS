f928748fd6143a330c98629b9b60dc22ef68fdf5
Manuj Chadha <manujchadha7777@gmail.com>
2026-02-04 21:34:50 +0530

    Time: 182 ms (22.73%) | Memory: 48.3 MB (53.63%) - LeetSync

--- 127-word-ladder/word-ladder.java
+++ 127-word-ladder/word-ladder.java
@@ -1,32 +1,25 @@
-public class Pair {
-    String first;
-    int second;
-    Pair (String first, int second){
-        this.first=first;
-        this.second=second;
-    }
-}
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        Queue<Pair> queue=new LinkedList<>();
-        queue.offer(new Pair(beginWord, 1));
-        Set<String> set=new HashSet<>();
-        for(int i=0;i<wordList.size();i++){
-            set.add(wordList.get(i));
-        }
+        Queue<String> queue=new LinkedList<>();
+        HashSet<String> set=new HashSet<>();
+        for(String word: wordList) set.add(word);
+        if(!set.contains(endWord)) return 0;
+        if(set.contains(beginWord)) set.remove(beginWord);
+        queue.add(beginWord);
+        int shortest=1;
         while(!queue.isEmpty()){
-            Pair element=queue.poll();
-            String word=element.first;
-            int steps=element.second;
-            if(word.equals(endWord)) return steps;
-            for(int i=0;i<word.length();i++){
-                for(char ch='a'; ch<='z'; ch++){
-                    char[] charArray=word.toCharArray();
-                    charArray[i]=ch;
-                    String str=new String(charArray);
-                    if(set.contains(str)){
-                        queue.offer(new Pair(str, steps+1));
-                        set.remove(str);
+            int n=queue.size();
+            shortest++;
+            for(int a=0;a<n;a++){
+                String word=queue.poll();
+                for(int i=0;i<word.length();i++){
+                    for(int j=0;j<26;j++){
+                        String formed=word.substring(0, i)+(char)('a'+j)+word.substring(i+1, word.length());
+                        if(set.contains(formed)){
+                            if(formed.equals(endWord)) return shortest;
+                            queue.add(formed);
+                            set.remove(formed);
+                        }
                     }
                 }
             }
