9d153419d84ea27d8ea767be42ff048a259ba281
Manuj Chadha <manujchadha7777@gmail.com>
2026-02-14 18:30:20 +0530

    Time: 4 ms (99.60%) | Memory: 63.3 MB (39.52%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.java
+++ 820-find-eventual-safe-states/find-eventual-safe-states.java
@@ -1,41 +1,27 @@
 class Solution {
     public List<Integer> eventualSafeNodes(int[][] graph) {
-        int[] safe=new int[graph.length];
-        boolean[] visited=new boolean[graph.length];
-        boolean[] pathVisited=new boolean[graph.length];
-        for(int i=0;i<graph.length;i++){
-            if(!visited[i]){
-                eventualSafe(graph, i, visited, pathVisited, safe);
+        int n=graph.length;
+        int[] state=new int[n];
+        List<Integer> ans=new ArrayList<>();
+        for(int i=0;i<n;i++) {
+            if(state[i]==0){
+                eventualSafe(i, graph, state);
             }
         }
-        List<Integer> ans=new ArrayList<>();
-        for(int i=0;i<safe.length;i++){
-            if(safe[i]==1) ans.add(i);
+        for(int i=0;i<state.length;i++){
+            if(state[i]==2) ans.add(i);
         }
         return ans;
     }
-    private boolean eventualSafe(int[][] graph, int i, boolean[] visited, 
-    boolean[] pathVisited, int[] safe){
-        visited[i]=true;
-        pathVisited[i]=true;
+    private boolean eventualSafe(int i, int[][] graph, int[] state) {
+        if(state[i]==1) return false;
+        if(state[i]==2) return true;
+        state[i]=1;
         int[] neighbors=graph[i];
         for(int neighbor: neighbors) {
-            if(!visited[neighbor]){
-                if(!eventualSafe(graph, neighbor, visited, pathVisited, safe)){
-                    safe[neighbor]=2;
-                    return false;
-                }
-            }
-            else {
-                if(pathVisited[neighbor]){
-                    safe[neighbor]=2;
-                    return false;
-                }
-                else if(safe[neighbor]==2) return false;
-            } 
+            if(!eventualSafe(neighbor, graph, state)) return false;
         }
-        safe[i]=1;
-        pathVisited[i]=false;
+        state[i]=2;
         return true;
     }
 }
