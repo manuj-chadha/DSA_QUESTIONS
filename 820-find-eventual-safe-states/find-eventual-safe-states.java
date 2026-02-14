6fd0da7f4a7677bf2122be0610401eaca8024abd
Manuj Chadha <manujchadha7777@gmail.com>
2026-02-14 18:11:40 +0530

    Time: 4 ms (99.60%) | Memory: 61.9 MB (59.48%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.java
+++ 820-find-eventual-safe-states/find-eventual-safe-states.java
@@ -1,31 +1,41 @@
 class Solution {
-    List<Integer> ans;
     public List<Integer> eventualSafeNodes(int[][] graph) {
-        ans=new ArrayList<>();
+        int[] safe=new int[graph.length];
         boolean[] visited=new boolean[graph.length];
         boolean[] pathVisited=new boolean[graph.length];
         for(int i=0;i<graph.length;i++){
             if(!visited[i]){
-                eventualSafe(graph, i, visited, pathVisited);
+                eventualSafe(graph, i, visited, pathVisited, safe);
             }
         }
-        Collections.sort(ans);
+        List<Integer> ans=new ArrayList<>();
+        for(int i=0;i<safe.length;i++){
+            if(safe[i]==1) ans.add(i);
+        }
         return ans;
     }
-    private boolean eventualSafe(int[][] graph, int i, boolean[] visited, boolean[] pathVisited){
+    private boolean eventualSafe(int[][] graph, int i, boolean[] visited, 
+    boolean[] pathVisited, int[] safe){
         visited[i]=true;
         pathVisited[i]=true;
         int[] neighbors=graph[i];
-        for(int a=0;a<neighbors.length;a++) {
-            if(!visited[neighbors[a]]){
-                if(!eventualSafe(graph, neighbors[a], visited, pathVisited)) return false;
+        for(int neighbor: neighbors) {
+            if(!visited[neighbor]){
+                if(!eventualSafe(graph, neighbor, visited, pathVisited, safe)){
+                    safe[neighbor]=2;
+                    return false;
+                }
             }
-            else if(pathVisited[neighbors[a]]){
-                return false;
-            }
+            else {
+                if(pathVisited[neighbor]){
+                    safe[neighbor]=2;
+                    return false;
+                }
+                else if(safe[neighbor]==2) return false;
+            } 
         }
+        safe[i]=1;
         pathVisited[i]=false;
-        ans.add(i);
         return true;
     }
 }
