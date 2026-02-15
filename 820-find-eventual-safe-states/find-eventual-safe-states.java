99379947b58e00a1d8a6989a01649e007066b498
Manuj Chadha <manujchadha7777@gmail.com>
2026-02-15 13:48:56 +0530

    Time: 23 ms (44.06%) | Memory: 61.4 MB (69.37%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.java
+++ 820-find-eventual-safe-states/find-eventual-safe-states.java
@@ -1,27 +1,39 @@
 class Solution {
     public List<Integer> eventualSafeNodes(int[][] graph) {
-        int n=graph.length;
-        int[] state=new int[n];
-        List<Integer> ans=new ArrayList<>();
-        for(int i=0;i<n;i++) {
-            if(state[i]==0){
-                eventualSafe(i, graph, state);
+        List<List<Integer>> adj=new ArrayList<>();
+        for(int i=0;i<graph.length;i++) {
+            adj.add(new ArrayList<>());
+        }
+        int[] indegree=new int[graph.length];
+        for(int i=0;i<graph.length;i++) {
+            int[] arr=graph[i];
+            for(int j=0;j<arr.length;j++){
+                adj.get(arr[j]).add(i);
+                indegree[i]++;
             }
         }
-        for(int i=0;i<state.length;i++){
-            if(state[i]==2) ans.add(i);
+        int[] safe=new int[graph.length];
+        Queue<Integer> queue=new LinkedList<>();
+        for(int i=0;i<indegree.length;i++){
+            if(indegree[i]==0) queue.add(i);
+        }
+        while(!queue.isEmpty()) {
+            int n=queue.size();
+            for(int i=0;i<n;i++){
+                int node=queue.poll();
+                safe[node]=1;
+                List<Integer> neighbors=adj.get(node);
+                for(Integer neighbor: neighbors) {
+                    if(--indegree[neighbor]==0){
+                        queue.add(neighbor);
+                    }
+                }
+            }
+        }
+        List<Integer> ans=new ArrayList<>();
+        for(int i=0;i<safe.length;i++){
+            if(safe[i]==1) ans.add(i);
         }
         return ans;
-    }
-    private boolean eventualSafe(int i, int[][] graph, int[] state) {
-        if(state[i]==1) return false;
-        if(state[i]==2) return true;
-        state[i]=1;
-        int[] neighbors=graph[i];
-        for(int neighbor: neighbors) {
-            if(!eventualSafe(neighbor, graph, state)) return false;
-        }
-        state[i]=2;
-        return true;
     }
 }
