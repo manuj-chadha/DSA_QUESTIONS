aa87ef83f18bc58eb421c6c09a75de2ae97b0a54
Manuj Chadha <manujchadha7777@gmail.com>
2026-02-19 08:43:12 +0530

    Time: 231 ms (17.82%) | Memory: 48.8 MB (39.25%) - LeetSync

--- 127-word-ladder/word-ladder.java
+++ 127-word-ladder/word-ladder.java
@@ -1,28 +1,33 @@
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        Queue<String> queue=new LinkedList<>();
         HashSet<String> set=new HashSet<>();
-        for(String word: wordList) set.add(word);
+        for(int i=0;i<wordList.size();i++){
+            set.add(wordList.get(i));
+        }
         if(!set.contains(endWord)) return 0;
-        if(set.contains(beginWord)) set.remove(beginWord);
+        Queue<String> queue=new LinkedList<>();
         queue.add(beginWord);
-        int shortest=1;
-        while(!queue.isEmpty()){
+        if(set.contains(beginWord)) set.remove(beginWord);
+        int len=1;
+        while (!queue.isEmpty()) {
             int n=queue.size();
-            shortest++;
-            for(int a=0;a<n;a++){
+            for(int a=0;a<n;a++) {
                 String word=queue.poll();
                 for(int i=0;i<word.length();i++){
-                    for(int j=0;j<26;j++){
-                        String formed=word.substring(0, i)+(char)('a'+j)+word.substring(i+1, word.length());
+                    for(int j=0;j<26;j++) {
+                        String formed=word.substring(0, i)+ (char) ('a'+j) + word.substring(i+1, word.length());
+                        if(formed.equals(endWord)){
+                            return len+1;
+                        }
                         if(set.contains(formed)){
-                            if(formed.equals(endWord)) return shortest;
                             queue.add(formed);
-                            set.remove(formed);
+                        System.out.println(formed);
+                            if(set.contains(formed)) set.remove(formed);
                         }
                     }
                 }
             }
+            if(!queue.isEmpty()) len++;
         }
         return 0;
     }
