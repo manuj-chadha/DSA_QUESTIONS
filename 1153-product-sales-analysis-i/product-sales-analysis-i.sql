477968b6a3f994554370aacbc2d552c88f8df027
Manuj Chadha <manujchadha7777@gmail.com>
2026-05-31 18:17:11 +0530

    Time: 1646 ms (5.01%) | Memory: 0B (100.00%) - LeetSync

--- 1153-product-sales-analysis-i/product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/product-sales-analysis-i.sql
@@ -1,2 +1,2 @@
 -- Write your PostgreSQL query statement below
-SELECT p.product_name, s.year, s.price FROM sales s LEFT JOIN product p ON s.product_id=p.product_id;
+SELECT p.product_name, s.year, s.price FROM sales s JOIN product p ON s.product_id=p.product_id;
