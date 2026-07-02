96a49e4fb7b2c32d2d8b269c095a02ac88a3a74d
Manuj Chadha <manujchadha7777@gmail.com>
2026-07-02 21:20:21 +0530

    Time: 2 ms (98.13%) | Memory: 43.6 MB (15.82%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,24 +1,18 @@
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> stack=new Stack<>();
-        for(int i=0;i<s.length();i++){
-            char ch=s.charAt(i);
-            if(ch=='(' || ch=='{' || ch=='[') stack.push(ch);
+        for(char ch: s.toCharArray()){
+            if(ch=='(' || ch=='[' || ch=='{'){
+                stack.push(ch);
+            }
             else {
-                if(ch==')'){
-                    if(!stack.isEmpty() && stack.peek()=='(') stack.pop();
-                    else return false;
-                }
-                else if(ch=='}'){
-                    if(!stack.isEmpty() && stack.peek()=='{') stack.pop();
-                    else return false;
-                }
-                else {
-                    if(!stack.isEmpty() && stack.peek()=='[') stack.pop();
-                    else return false;
-                }
+                if(stack.isEmpty()) return false;
+                if(ch==')' && stack.pop()!='(') return false;
+                else if(ch==']' && stack.pop()!='[') return false;
+                else if(ch=='}' && stack.pop()!='{') return false;
             }
         }
-        return stack.isEmpty();
+        if(!stack.isEmpty()) return false;
+        return true;
     }
 }
